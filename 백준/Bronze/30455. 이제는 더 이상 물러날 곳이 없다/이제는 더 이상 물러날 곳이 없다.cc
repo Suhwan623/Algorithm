6f47cf4843ceaa66c1ea173b780420ddee6d3da5
Suhwan Lee <133092014+Suhwan623@users.noreply.github.com>
2024-05-17 13:52:52 +0900

    [Bronze I] Title: 이제는 더 이상 물러날 곳이 없다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30455. 이제는 더 이상 물러날 곳이 없다/이제는 더 이상 물러날 곳이 없다.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n % 2 == 0) {
+            cout << "Duck";
+     }
+    else if (n % 2 == 1) {
+            cout << "Goose";
+      }
+    return 0;
+}
