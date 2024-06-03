60a784f32352406a9c04b85bf735f32a3cdfb036
Suhwan Lee <133092014+Suhwan623@users.noreply.github.com>
2024-06-03 17:28:16 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string num;
+    cin >> num;
+
+    sort(num.begin(), num.end(), greater<char>());
+
+    cout << num << endl;
+
+    return 0;
+}
