e7ff45d6e4456111bdd6bb276c281e46ea19bad4
Suhwan Lee <133092014+Suhwan623@users.noreply.github.com>
2025-05-20 11:57:12 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a;
+    cin >> b;
+    int extent = a * b;
+    cout << extent << endl;
+    return 0;
+}
