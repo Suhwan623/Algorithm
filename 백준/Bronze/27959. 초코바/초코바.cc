69e28b6f62e56a7912e8464902e2d4482e2be338
Suhwan Lee <133092014+Suhwan623@users.noreply.github.com>
2025-05-21 09:00:58 +0900

    [Bronze V] Title: 초코바, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    if (n * 100 >= m) {
+        cout << "Yes";
+    }
+    else if (n * 100 <= m) {
+        cout << "No";
+    }
+}
